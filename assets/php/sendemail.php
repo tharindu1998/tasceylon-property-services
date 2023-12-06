833ba7c7f2c7e08c6949bcb38b869aa0cc0cd393
tharindu1998 <tharindudulshanfdo@gmail.com>
2023-12-06 20:50:06 +0530

    email sender

---
+++ assets/php/sendemail.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Set recipient email address
+    $to = "tharindudulshanfdo@gmail.com";
+
+    // Set email subject
+    $email_subject = "New Contact Form Submission: $subject";
+
+    // Build the email body
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Phone: $phone\n\n";
+    $email_body .= "Message:\n$message";
+
+    // Set headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send the email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Thank you for your message. We will get back to you soon.";
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
+} else {
+    // Handle invalid request method
+    echo "Invalid request method.";
+}
+?>
